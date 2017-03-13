db5fe320587e506161317fb682fe253225784e3e
04050582 <hc920052@gmail.com>
2017-03-13 21:04:01 +0800

    Add files via upload

---
+++ lab01.ino
@@ -0,0 +1,31 @@
+const byte ledPin = 13;
+
+void setup() 
+{
+Serial.begin(9600);
+Serial.println("Hello,World");
+Serial.print("\tLED Pin is: ");
+Serial.print(ledPin);
+Serial.print("\n");
+pinMode(ledPin,OUTPUT);
+}
+
+int val;
+void loop() 
+{
+if( Serial.available() )
+{
+  val=Serial.read();
+  switch(val)
+  {
+    case'0':
+    digitalWrite(ledPin,LOW);
+     Serial.println("LED OFF");
+     break;
+     case'1':
+      digitalWrite(ledPin,HIGH);
+     Serial.println("LED ON");
+     break;
+  }
+}
+}
---
+++ lab02.ino
@@ -0,0 +1,14 @@
+void setup() {
+Serial.begin(9600);
+
+}
+
+void loop() {
+int i;
+float Analogin,Vin;
+for(i=1;i<=5;i++)
+Analogin=Analogin*0.7 + analogRead(A0)*0.3;
+Vin=Analogin*5/1024;
+Serial.println(Vin);
+delay(500);
+}
